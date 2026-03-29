c8b87b8c16755740024facc7533a76eb7aadec27
BetsiMe <betsimedina65@gmail.com>
2026-03-29 12:50:25 -0500

    primer commit

---
+++ src/main/java/ec/edu/espoch/taller2/TALLER2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ec.edu.espoch.taller2;
+
+/**
+ *
+ * @author Admin
+ */
+public class TALLER2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
